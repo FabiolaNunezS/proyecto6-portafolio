7263fc55649c6da2048a8f2aea08c4bc7dd29c99
Fabiola Nuñez <fabyns@hotmail.com>
2023-10-01 12:12:05 -0300

    about

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
-            Acerca
+            Un poco Sobre...
           </p>
           <h2 className="py-4">DE QUIEN SOY</h2>
           <p className="py-2 text-gray-600">
--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -10,13 +10,9 @@ const ProjectItem = ({ tittle, backgroundImg, projectUrl }) => {
         src={backgroundImg}
         alt="/"
       />
-      <div className="hiden group-hover:block absolute top-[50%] left-[50%] transalete-x-[50%] translate-y-[-50%]">
-        <h3 className="text-2xl text-white tracking-wider text-center">
-          {tittle}
-        </h3>
-        <p className="pb-4 pt-2 text-white text-center">
-          React, Node Js, Bootstrap
-        </p>
+      <div className="hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h3 className="text-2xl text-white tracking-wider">{tittle}</h3>
+        <p className="pb-4 pt-2 text-white">React, Node Js, Bootstrap</p>
         <Link href={projectUrl}>
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             Más Información
